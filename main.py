e04cc3a50212754cb05bbf99391e99a8f7c9b7a0
Kendall <vedaker@st.ug.edu.gh>
2024-12-08 19:04:50 +0000

    Changed naming of files and made other minor changes

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ import integratedSystem
 
 def main():
   
-    io_manager = IOManager()
-    memory_manager = MemoryManager(frames=3)
-    unified_system = UnifiedSystem(io_manager, memory_manager)
+    io_manager = Osystem()
+    memory_manager = memoryManagementSystem(frames=3)
+    unified_system = integratedSystem(io_manager, memory_manager)
     
-    # Add requests and run the simulation
+   
     unified_system.add_request("Disk", "R1", 0, 20, [2, 3, 1, 5])
     unified_system.add_request("Printer", "R2", 10, 25, [1, 4, 2, 3])
     unified_system.run()
     
-    # Evaluate performance
+
     performance = unified_system.evaluate_performance()
     print("Performance Metrics:", performance)
 
